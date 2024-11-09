9ec1d58922df984692ca18038c4df288ef8ca28f
Std137 <Std137@bk.ru>
2024-11-09 21:50:12 +0700

    refresh
    convert tab to space;
    add include section;
    Fixed bugs in FSM_test.

--- src/FPGA_tester/src/fsm.sv
+++ src/FPGA_tester/src/fsm.sv
@@ -1,114 +1,116 @@
-module fsm(
-    input logic in_clk,                // Системный тактовый сигнал
-    input logic in_rst,              	// Сброс (активный низкий)
-    input logic in_push_sw,            // Нажата кнопка switch
-    input logic in_push_rst,           // Нажата кнопка reset
-	 input logic in_mem_w_rd,				// Флаг, запись в память окончена
-	 input logic in_utx_s_rd,				// Флаг, uart_tx отправка данных окончена
-	 input logic in_utx_s_bs,				// Флаг, uart_tx занят
-	 input logic [5:0] in_mem,				// Шина данных чтения памяти
-	 input logic [7:0] in_urx,				// Шина данных чтения uart_rx
-	 output wire [5:0] out_mem,			// Шина данных записи в память
-	 output logic out_mem_w_en,			// Флаг, запись в память разрешена
-	 output logic out_utx_s_en,			// Флаг, отправка данных разрешена
-	 output logic out_rst					// Флаг, произвести сброс
-);
+`include "fsm1_pkg_b.sv"
+import fsm1_pkg::*;
 
-typedef enum logic [2:0]
-    { 
-        STATE_IDLE  	 = 3'b000,
-        STATE_WRITE_C = 3'b001,
-		  STATE_WRITE_S =	3'b010,
-        STATE_RESET 	 = 3'b011,
-		  STATE_WAIT 	 = 3'b100,
-		  STATE_SEND  	 = 3'b101,
-		  XXX 			 = 'x
-	 } state_e;
+module fsm(
+    input logic in_clk,                 // Системный тактовый сигнал
+    input logic in_rst,                 // Сброс (активный низкий)
+    input logic in_push_sw,             // Нажата кнопка switch
+    input logic in_push_rst,            // Нажата кнопка reset
+    input logic in_mem_w_rd,            // Флаг, запись в память окончена
+    input logic in_utx_s_rd,            // Флаг, uart_tx отправка данных окончена
+    input logic in_utx_s_bs,            // Флаг, uart_tx занят
+    input logic [5:0] in_mem,           // Шина данных чтения памяти
+    input logic [7:0] in_urx,           // Шина данных чтения uart_rx
+    output logic [5:0] out_mem,         // Шина данных записи в память
+    output logic out_mem_w_en,          // Флаг, запись в память разрешена
+    output logic out_utx_s_en,          // Флаг, отправка данных разрешена
+    output logic out_rst                // Флаг, произвести сброс
+);
 
 state_e current_state, next_state;
 
-logic in_cmd_setup, in_cmd_reset, in_cmd_send, in_key_tr, in_rx_tr; 	
+logic in_cmd_setup, in_cmd_reset, in_cmd_send, in_key_tr, in_rx_tr;  
 
 assign in_cmd_setup = (in_urx[7:6] == 2'b10)?'1:'0; // Получена команда записать значения
 assign in_cmd_reset = (in_urx[7:6] == 2'b11)?'1:'0; // Получена команда сброса
 assign in_cmd_send  = (in_urx[7:6] == 2'b01)?'1:'0; // Получена команда отправить состояние
-assign out_mem = (in_key_tr)?{~in_mem[5],in_mem[4:0]}:(in_rx_tr)?in_urx[5:0]:in_mem[5:0];
 
-    always_ff @(posedge in_clk, negedge in_rst) begin
-        if (!in_rst)
-            current_state <= STATE_IDLE;
-        else 
-            current_state <= next_state; 
+assign out_mem = (in_key_tr)?{~in_mem[5],in_mem[4:0]}:(in_rx_tr)?in_urx[5:0]:in_mem[5:0];
+  
+  always_ff @(posedge in_clk, negedge in_rst) begin
+    if (!in_rst)
+      current_state <= STATE_IDLE;
+    else 
+      current_state <= next_state; 
+  end
+  
+  always_comb begin
+    next_state = XXX;
+    case (current_state)
+      STATE_IDLE:    if       (in_push_sw)   next_state = STATE_WRITE_S;
+                     else if  (in_cmd_send)  next_state = STATE_WAIT;
+                     else if  (in_cmd_setup) next_state = STATE_WRITE_C;
+                     else if  (in_cmd_reset) next_state = STATE_RESET;
+                     else if  (in_push_rst)  next_state = STATE_RESET;
+                     else                    next_state = STATE_IDLE;
+
+      STATE_WRITE_C: if       (in_mem_w_rd)  next_state = STATE_WAIT;
+                     else                    next_state = STATE_WRITE_C;
+      STATE_WRITE_S: if       (in_mem_w_rd)  next_state = STATE_WAIT;
+                     else                    next_state = STATE_WRITE_S;
+      STATE_RESET:                           next_state = STATE_IDLE;
+      STATE_WAIT:    if       (!in_utx_s_bs) next_state = STATE_SEND;
+                     else                    next_state = STATE_WAIT;
+      STATE_SEND:    if       (in_utx_s_rd)  next_state = STATE_IDLE;
+                     else                    next_state = STATE_SEND;
+      default:                               next_state = XXX;
+    endcase
+  end
+ 
+  always_comb begin
+    if (!in_rst) begin 
+      out_mem_w_en = 0;
+      out_utx_s_en = 0;
+      out_rst    = 0;
+      in_key_tr = 0;
+      in_rx_tr = 0;
     end
-	 
-	 always_comb begin
-		  next_state = XXX;
-        case (current_state)
-				STATE_IDLE: 	if  		(in_push_sw) 	next_state = STATE_WRITE_S;
-									else if 	(in_cmd_send) 	next_state = STATE_SEND;
-									else if 	(in_cmd_setup) next_state = STATE_WRITE_C;
-									else if 	(in_cmd_reset) next_state = STATE_RESET;
-									else if 	(in_push_rst)	next_state = STATE_RESET;
-									else 							next_state = STATE_IDLE;
-				
-            STATE_WRITE_C: if 		(in_mem_w_rd)	next_state = STATE_WAIT;
-									else							next_state = STATE_WRITE_C;
-				STATE_WRITE_S: if 		(in_mem_w_rd) 	next_state = STATE_WAIT;
-									else 			 				next_state = STATE_WRITE_S;
-            STATE_RESET:  									next_state = STATE_IDLE;
-				STATE_WAIT: 	if 		(!in_utx_s_bs)	next_state = STATE_SEND;
-									else							next_state = STATE_WAIT;
-				STATE_SEND: 	if 		(in_utx_s_rd)	next_state = STATE_IDLE;
-									else							next_state = STATE_SEND;
-				default:											next_state = XXX;
-			endcase
-		end
-	
-	 always_comb begin
-			case(current_state)
-					STATE_IDLE: 	begin	
-											out_mem_w_en = 0;
-											out_utx_s_en = 0;
-											out_rst 		 = 0;
-											in_key_tr = 0;
-											in_rx_tr = 0;
-										end
-					STATE_WRITE_C: begin
-											out_utx_s_en = 0;
-											out_rst 		 = 0;
-											out_mem_w_en = 1;
-											in_key_tr = 0;
-											in_rx_tr = 1;
-										end
-					STATE_WRITE_S: begin
-											out_utx_s_en = 0;
-											out_rst 		 = 0;
-											out_mem_w_en = 1;
-											in_key_tr = 1;
-											in_rx_tr = 0;
-										end
-					STATE_RESET: 	begin
-											out_mem_w_en = 0;
-											out_utx_s_en = 0;
-											out_rst 		 = 1;
-											in_key_tr = 0;
-											in_rx_tr = 0;
-										end
-					STATE_WAIT: 	begin
-											out_mem_w_en = 0;
-											out_rst 		 = 0;
-											out_utx_s_en = 1;
-											in_key_tr = 0;
-											in_rx_tr = 0;
-										end
-					STATE_SEND: 	begin
-											out_mem_w_en = 0;
-											out_rst 		 = 0;
-											out_utx_s_en = 1;
-											in_key_tr = 0;
-											in_rx_tr = 0;
-										end
-					default: 		  {in_rx_tr, in_key_tr, out_mem_w_en, out_utx_s_en, out_rst} = 'x;
-			endcase
-		end
+    else begin
+      case(current_state)
+        STATE_IDLE:  begin 
+          out_mem_w_en = 0;
+          out_utx_s_en = 0;
+          out_rst    = 0;
+          in_key_tr = 0;
+          in_rx_tr = 0;
+        end
+        STATE_WRITE_C: begin
+          out_utx_s_en = 0;
+           out_rst    = 0;
+          out_mem_w_en = 1;
+          in_key_tr = 0;
+          in_rx_tr = 1;
+        end
+        STATE_WRITE_S: begin
+          out_utx_s_en = 0;
+          out_rst    = 0;
+          out_mem_w_en = 1;
+          in_key_tr = 1;
+          in_rx_tr = 0;
+        end
+        STATE_RESET:  begin
+          out_mem_w_en = 0;
+          out_utx_s_en = 0;
+          out_rst    = 1;
+          in_key_tr = 0;
+          in_rx_tr = 0;
+        end
+        STATE_WAIT:  begin
+          out_mem_w_en = 0;
+          out_rst    = 0;
+          out_utx_s_en = 1;
+          in_key_tr = 0;
+          in_rx_tr = 0;
+        end
+        STATE_SEND:  begin
+          out_mem_w_en = 0;
+          out_rst    = 0;
+          out_utx_s_en = 1;
+          in_key_tr = 0;
+          in_rx_tr = 0;
+        end
+        default:{in_rx_tr, in_key_tr, out_mem_w_en, out_utx_s_en, out_rst} = 'x;
+      endcase
+    end
+  end
 endmodule
--- src/FPGA_tester/src/fsm_tb.sv
+++ src/FPGA_tester/src/fsm_tb.sv
@@ -1,17 +1,20 @@
+`include "fsm1_pkg_b.sv"
+import fsm1_pkg::*;
+
 module fsm_tb();
 logic in_clk = 0;                // Системный тактовый сигнал
-logic in_rst = 1;              	// Сброс (активный низкий)
+logic in_rst = 1;                // Сброс (активный низкий)
 logic in_push_sw = 0;            // Нажата кнопка switch
-logic in_push_rst = 0;          // Нажата кнопка reset
-logic in_mem_w_rd = 0;				// Флаг, запись в память окончена
-logic in_utx_s_rd = 0;				// Флаг, uart_tx отправка данных окончена
-logic in_utx_s_bs = 0;				// Флаг, uart_tx занят
-logic [5:0] in_mem = 6'b110101;				// Шина данных чтения памяти
-logic [7:0] in_urx = '0;				// Шина данных чтения uart_rx
-wire [5:0] out_mem = '0;			// Шина данных записи в память
-wire out_mem_w_en = 0;			// Флаг, запись в память разрешена
-wire out_utx_s_en = 0;			// Флаг, отправка данных разрешена
-wire out_rst = 0;					// Флаг, произвести сброс
+logic in_push_rst = 0;           // Нажата кнопка reset
+logic in_mem_w_rd = 0;           // Флаг, запись в память окончена
+logic in_utx_s_rd = 0;           // Флаг, uart_tx отправка данных окончена
+logic in_utx_s_bs = 1;           // Флаг, uart_tx занят
+logic [5:0] in_mem = 6'b110101;  // Шина данных чтения памяти
+logic [7:0] in_urx = '0;         // Шина данных чтения uart_rx
+wire [5:0] out_mem;              // Шина данных записи в память
+wire out_mem_w_en;               // Флаг, запись в память разрешена
+wire out_utx_s_en;               // Флаг, отправка данных разрешена
+wire out_rst;                    // Флаг, произвести сброс
 
 
 fsm fsm(.*);
@@ -19,23 +22,25 @@ fsm fsm(.*);
   always
     #1  in_clk = ~in_clk;
 
-initial begin
-    #4 in_rst = 0;
+  initial begin
+    #3 in_rst = 0;
     #4 in_rst = 1;
     #4 in_urx = 8'b01110101;
+    #4 in_utx_s_bs = 0;
     #4 in_utx_s_rd = 1;
-    #4 in_urx = '0;
-		 in_utx_s_rd = 0;
+       in_utx_s_bs = 1;
+       in_urx = '0;
+    #4 in_utx_s_rd = 0;
     #4 in_push_sw=1;
     #4 in_mem_w_rd=1;
     #4 in_push_sw=0;
-		 in_mem_w_rd=1;
+       in_mem_w_rd=1;
     #10 $finish;
   end
 
   initial begin
     $dumpfile("fsm_tb.vcd");
-    $dumpvars(0,fsm);
+    $dumpvars();
   end
 
 endmodule
