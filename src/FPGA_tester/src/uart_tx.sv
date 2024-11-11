480bdd20e10f3ebf9b172a39e5762b07cd46ea20
Std137 <61679468+Std137@users.noreply.github.com>
2024-11-11 19:56:38 +0700

    11.11

--- src/FPGA_tester/src/uart_tx.sv
+++ src/FPGA_tester/src/uart_tx.sv
@@ -1,128 +1,124 @@
 module uart_tx
 #(
-		parameter TICKS_PER_BIT = 243,
-		parameter TICKS_PER_BIT_SIZE = 8
+  parameter TICKS_PER_BIT = 243,
+  parameter TICKS_PER_BIT_SIZE = 8
 )
 (
-		input i_clk,
-		input i_start,
-		input [7:0] i_data,
-		output wire o_done,
-		output wire o_busy,
-		output wire o_dout
+  input i_clk,
+  input i_start,
+  input [7:0] i_data,
+  output wire o_done,
+  output wire o_busy,
+  output wire o_dout
 );
 
-localparam	STATE_IDLE 			= 5'b00001,
-			STATE_SEND_START 	= 5'b00010,
-			STATE_SEND_BITS		= 5'b00100,
-			STATE_SEND_STOP		= 5'b01000,
-			STATE_DONE			= 5'b10000;
-	
-logic [4:0] currentState, nextState;
+typedef enum logic [1:0]
+    { 
+        STATE_IDLE       = 2'b00,
+        STATE_SEND_BITS  = 2'b01,
+        STATE_SEND_WAIT  = 2'b10,
+        STATE_DONE       = 2'b11,
+        XXX              = 'x
+  } state_e;
+
+state_e currentState, nextState;
+
 logic done_flag;
 logic busy_flag;
 logic tx_output;
-	
+ 
 assign o_dout = tx_output;
 assign o_done = done_flag;
 assign o_busy = busy_flag;
-	
+
 logic [7:0] tx_reg;
 logic [3:0] tx_bit_counter;
 logic [TICKS_PER_BIT_SIZE-1:0] ticks_counter;
 
-wire ticks_counter_ovf 		= (ticks_counter == TICKS_PER_BIT-1);
-wire tx_bit_counter_ovf 	= (tx_bit_counter[3]); // if equals >= 8
+wire ticks_counter_ovf   = (ticks_counter == TICKS_PER_BIT-1);
+wire tx_bit_counter_ovf  = (tx_bit_counter == 4'b1010);
 
-always @(*) begin
-	case(currentState)
-			default: 
-                begin 
-                    nextState = STATE_IDLE;
-                    done_flag = 0;
-                    busy_flag = 0;
-                    tx_output = 1;
-                end
-			STATE_IDLE: 
-                begin 
-                    done_flag = 0;
-                    busy_flag = 0;
-                    tx_output = 1;
-                    if(i_start)
-                        nextState = STATE_SEND_START;
-                    else
-                        nextState = STATE_IDLE;
-                end
-		
-            STATE_SEND_START: 
-                begin 
-                    done_flag = 0;
-                    busy_flag = 1;
-                    tx_output = 0;			
-                    if(ticks_counter_ovf)
-                        nextState = STATE_SEND_BITS;
-                    else
-                        nextState = STATE_SEND_START;
-                    end
-		
-            STATE_SEND_BITS: 
-                begin 
-                    done_flag = 0;
-                    busy_flag = 1;
-                    tx_output = tx_bit_counter_ovf ? 1'b1 : tx_reg[0];
-                    if(tx_bit_counter_ovf)
-                        nextState = STATE_SEND_STOP;
-                    else
-                        nextState = STATE_SEND_BITS;
-				end
-		
-            STATE_SEND_STOP: 
-                begin 
-                    done_flag = 0;
-                    busy_flag = 1;
-                    tx_output = 1;
-                    if(ticks_counter_ovf)
-                        nextState = STATE_DONE;
-                    else
-                        nextState = STATE_SEND_STOP;
-                end
-		
-            STATE_DONE: 
-                begin 
-                    done_flag = 1;
-                    busy_flag = 1;
-                    tx_output = 1;
-                    nextState = STATE_IDLE;
-                end
-		
-	endcase
+always_ff @(posedge in_clk, negedge in_rst) begin
+    if (!in_rst)
+      current_state <= STATE_IDLE;
+    else 
+      current_state <= next_state; 
+  end
+
+always_comb begin
+  next_State = XXX;
+  case(currentState)
+    STATE_IDLE:       if    (i_start)            nextState = STATE_SEND_START;
+                      else                       nextState = STATE_IDLE;
+    STATE_SEND_BITS:  if    (tx_bit_counter_ovf) nextState = STATE_SEND_DONE;
+                      else                       nextState = STATE_SEND_WAIT;
+    STATE_SEND_WAIT:  if    (ticks_counter_ovf)  nextState = STATE_SEND_BITS;
+                      else                       nextState = STATE_SEND_WAIT;
+    STATE_DONE:                                  nextState = STATE_IDLE;
+    default:                                     nextState = XXX;
+ endcase
 end
 
-always @(posedge i_clk) 
-        begin 
-            currentState <= nextState;
-            if (currentState == STATE_SEND_START || 
-                currentState == STATE_SEND_BITS || 
-                currentState == STATE_SEND_STOP) 
-                begin
-                    if(ticks_counter_ovf) ticks_counter <= 0;
-                    else ticks_counter <= ticks_counter + 1;
-                end
-            if (currentState == STATE_SEND_BITS) 
-                begin 
-                    if(ticks_counter_ovf) 
-                        begin
-                            tx_bit_counter <= tx_bit_counter + 1;
-                            tx_reg <= tx_reg >> 1;
-                        end
-                end
-            if(currentState == STATE_IDLE) 
-                begin 
-                    if(i_start) 
-                        begin 
-                            tx_reg <= i_data;
-                            tx_bit_counter <= 0;
-                        end
-                end
+
+always_ff @(posedge in_clk) begin
+  if (currentState == STATE_SEND_WAIT)
+    if (ticks_counter_ovf) ticks_counter <= 0;
+    else ticks_counter <= ticks_counter + 1;
+  end
+
+always_ff @(posedge in_clk) begin
+  if (currentState == STATE_SEND_BITS)
+    if (tx_bit_counter_ovf) tx_bit_counter <= 0;
+    else tx_bit_counter <= tx_bit_counter + 1;
+  end
+
+always_comb begin
+    if (!in_rst) begin 
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = '0;
+          tx_reg    = '0;
+    end
+    else begin
+    case(current_state)
+        STATE_IDLE:  begin 
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = '0;
+          tx_reg    = {0,i_data,1};
         end
+        STATE_SEND_BITS: begin
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = tx_reg[0];
+          tx_reg    = tx_reg;
+        end
+        STATE_SEND_WAIT: begin
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = tx_reg[0];;
+          tx_reg    = tx_reg;
+        end
+        STATE_RESET:  begin
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = '0;
+          tx_reg    = i_data;
+        end
+        STATE_WAIT:  begin
+          done_flag = '0;
+          busy_flag = '0;
+          tx_output = '0;
+          tx_reg    = i_data;
+        end
+        STATE_DONE:  begin
+          done_flag = '1;
+          busy_flag = '0;
+          tx_output = '1;
+          tx_reg    = '0;
+        end
+        default:{in_rx_tr, in_key_tr, out_mem_w_en, out_utx_s_en, out_rst} = 'x;
+      endcase
+    end
+
 endmodule 
